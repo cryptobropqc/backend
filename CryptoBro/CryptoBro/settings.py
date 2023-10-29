4f0dd65d3d44d47cc36f54495fb6fc1a5a97ebb5
Sergei Kalinichenko <134352849+sergeikalinichenko@users.noreply.github.com>
2023-10-29 12:51:25 +0100

    debug false

--- CryptoBro/CryptoBro/settings.py
+++ CryptoBro/CryptoBro/settings.py
@@ -12,13 +12,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%&g08@fa173axmj&5m_@in$)t)56m46ud!ykhgxyt#$f3+uv0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-    'testserver',
+ALLOWED_HOSTS = [    
     '52.91.232.233',
     'api.cryptobro.pro',
 ]
