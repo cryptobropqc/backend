ab45fa3c32bbddbfa04aec3cb2bb8471cb243a7a
Sergei Kalinichenko <134352849+sergeikalinichenko@users.noreply.github.com>
2023-11-05 13:13:09 +0100

    media update

--- CryptoBro/CryptoBro/settings.py
+++ CryptoBro/CryptoBro/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%&g08@fa173axmj&5m_@in$)t)56m46ud!ykhgxyt#$f3+uv0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
