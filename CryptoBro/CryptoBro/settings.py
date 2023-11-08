7b8de81eb92f47e60be70ecad5ca89b59a1d1250
Sergei Kalinichenko <134352849+sergeikalinichenko@users.noreply.github.com>
2023-11-08 22:10:23 +0100

    settings update

--- CryptoBro/CryptoBro/settings.py
+++ CryptoBro/CryptoBro/settings.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
